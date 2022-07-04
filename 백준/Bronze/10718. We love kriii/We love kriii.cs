07078f4598b50f316943a2e770c4931e4a593998
MoHoDu <imvagueness@gmail.com>
2022-07-05 08:58:29 +0900

    [Bronze V] Title: We love kriii, Time: 56 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BOJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군\n강한친구 대한육군");
+        }
+    }
+}
