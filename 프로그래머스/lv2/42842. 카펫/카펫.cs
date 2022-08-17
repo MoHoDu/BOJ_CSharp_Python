3eb3d779213fce46cce740182fb5597320faa8e0
MoHoDu <imvagueness@gmail.com>
2022-08-17 16:10:55 +0900

    [level 2] Title: 카펫, Time: 0.33 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.cs
+++ 프로그래머스/lv2/42842. 카펫/카펫.cs
@@ -6,26 +6,26 @@ using System.Linq;
 
 public class Solution
 {
-    public int[] solution(int brown, int yellow)
-    {
-        int[] answer = new int[2];
+   public int[] solution(int brown, int yellow)
+   {
+       int[] answer = new int[2];
 
-        int sumN = brown + yellow;
+       int sumN = brown + yellow;
 
-        for (int i = 1; i <= MathF.Sqrt(sumN); i++)
-        {
-            if (sumN % i == 0)
-            {
-                int a = (sumN / i);
-                int b = i;
-                if ((a - 2) * (b - 2) == yellow)
-                {
-                    answer[0] = (int)MathF.Max(a, b);
-                    answer[1] = (int)MathF.Min(a, b);
-                }
-            }
-        }
+       for (int i = 1; i <= MathF.Sqrt(sumN); i++)
+       {
+           if (sumN % i == 0)
+           {
+               int a = (sumN / i);
+               int b = i;
+               if ((a - 2) * (b - 2) == yellow)
+               {
+                   answer[0] = (int)MathF.Max(a, b);
+                   answer[1] = (int)MathF.Min(a, b);
+               }
+           }
+       }
 
-        return answer;
-    }
+       return answer;
+   }
 }
